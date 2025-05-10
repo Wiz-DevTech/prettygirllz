42dbb86b1a43ceadf070c6735f925f6cb3a15dbc
Teri Roland <Teriroland15@gmail.com>
2025-05-10 09:34:49 -0600

    Delete social-commerce-service directory

--- social-commerce-service/config/db-init.sql
+++
@@ -1,7 +0,0 @@
--- Database Initialization Script
-CREATE DATABASE social_commerce;
-CREATE USER social_commerce_user WITH PASSWORD 'password';
-GRANT ALL PRIVILEGES ON DATABASE social_commerce TO social_commerce_user;
-
--- Create schemas if needed
-CREATE SCHEMA IF NOT EXISTS social_commerce;
